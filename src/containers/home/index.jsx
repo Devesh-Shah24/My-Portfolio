671d2d8dd40b723214dc1758586b50f600c5a2c8
Devesh-Shah24 <deveshmuz2020@gmail.com>
2025-04-20 00:40:53 +0530

    Initial commit

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import profileImg from "../../assets/profile.jpg";
 import './styles.scss';
 import { Animate } from "react-simple-animate"
-import '@fortawesome/fontawesome-free/css/all.min.css';//
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { SiFreelancer } from "react-icons/si";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 
 const Home = () => {
@@ -16,38 +18,38 @@ const Home = () => {
                     <h3>Hi, my name is</h3>
                     <h1>Devesh Kumar</h1>
                     <p>
-                    "Learning. Building. Growing. Always forward". 
-                    I have built several industry-grade projects from scratch, showcasing strong skills in front-end development, responsive design, and modern web technologies like React, JavaScript, HTML, SCSS, and more. As a B.Tech Computer Science student, I’ve gained hands-on experience through real-world projects and continue to sharpen my skills daily. I'm currently exploring full-stack development and aiming to master MERN stacks with Django to become a versatile developer ready for any challenge.
+                        "Learning. Building. Growing. Always forward".
+                        I have built several industry-grade projects from scratch, showcasing strong skills in front-end development, responsive design, and modern web technologies like React, JavaScript, HTML, SCSS, and more. As a B.Tech Computer Science student, I’ve gained hands-on experience through real-world projects and continue to sharpen my skills daily. I'm currently exploring full-stack development and aiming to master MERN stacks with Django to become a versatile developer ready for any challenge.
                     </p>
 
                     <div className="hero_stats">
                         <div className="hero_stat-box">
-                            <h2>15</h2>
+                            <h2>05+</h2>
                             <span>VERIFIED SKILLS</span>
                         </div>
                         <div className="hero_stat-box">
-                            <h2>8</h2>
+                            <h2>10+</h2>
                             <span>PROFESSIONAL PROJECTS</span>
                         </div>
                         <div className="hero_stat-box">
-                            <h2>175+</h2>
+                            <h2>100+</h2>
                             <span>DSA PROBLEMS SOLVED</span>
                         </div>
                     </div>
-                    <Animate/>
+                    <Animate />
                     <div className="header-actions">
                         <div className="social-icons">
                             <a href="https://www.linkedin.com/in/deveshkumar1" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-linkedin"></i>
+                                {<FaLinkedin size={35} />}
                             </a>
                             <a href="https://github.com/Devesh-Shah24" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-github"></i>
+                                {<FaGithub size={35} />}
                             </a>
                             <a href="https://www.instagram.com/mr.devesh__shah" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-instagram"></i>
+                                {<FaInstagram size={35} />}
                             </a>
-                            <a href="https://www.facebook.com/share/1Bf8PpaWuJ/" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-facebook"></i>
+                            <a href="https://www.freelancer.com/u/deveshk03?sb=t" target="_blank" rel="noopener noreferrer">
+                                {<SiFreelancer size={35} />}
                             </a>
                         </div>
                         <div className='button'>
--- src/containers/project/index.jsx
+++ src/containers/project/index.jsx
@@ -1,112 +1,61 @@
-import React from "react";
-import PageHeaderContent from "../../components/pageHeaderContent";
-import { AiFillProject } from "react-icons/ai";
-import ImageOne from '../../images/image-2.png'
-import ImageTwo from '../../images/image-3.jpg'
-import ImageThree from '../../images/image-4.png'
-import ImageFour from '../../images/image-5.png'
-import ImageFive from '../../images/image-6.png'
-import "./styles.scss"
-import { useState } from "react";
-
-const portfolioData = [
+export const skillsData = [
     {
-        id: 2,
-        name: "Portfolio",
-        image: ImageOne,
-        link: ''
-    },
-    {
-        id: 3,
-        name: "Snake Game",
-        image: ImageTwo,
-        link: ''
-    },
-    {
-        id: 2,
-        name: "Todo List",
-        image: ImageThree,
-        link: ''
+        label: "Front End",
+        data: [
+            {
+                skillName: "HTML",
+                Perscentage: "89",
+            },
+            {
+                skillName: "CSS",
+                Perscentage: "80",
+            },
+            {
+                skillName: "SCSS",
+                Perscentage: "89",
+            },
+            {
+                skillName: "JavaScript",
+                Perscentage: "90",
+            },
+            {
+                skillName: "React",
+                Perscentage: "89",
+            },
+        ],
     },
     // {
-    //     id: 2,
-    //     name: "Todo App",
-    //     image: ImageFour,
-    //     link: ''
+    //     label: "Back End",
+    //     data: [
+    //         {
+    //             skillName: "Node.JS",
+    //             Perscentage: "75",
+    //         },
+    //         {
+    //             skillName: "Express",
+    //             Perscentage: "79",
+    //         },
+    //         // {
+    //         //     skillName: "Django",
+    //         //     Perscentage: "90",
+    //         // },
+    //     ],
     // },
-    {
-        id: 2,
-        name: "Ecommerce",
-        image: ImageFive,
-        link: ''
-    },
+    // {
+    //     label: "DataBase",
+    //     data: [
+    //         // {
+    //         //     skillName: "SQL",
+    //         //     Perscentage: "90",
+    //         // },
+    //         {
+    //             skillName: "Mongo DB",
+    //             Perscentage: "85",
+    //         },
+    //         {
+    //             skillName: "No-SQL",
+    //             Perscentage: "80",
+    //         },
+    //     ],
+    // },
 ];
-
-
-const filterData = [
-    {
-        filterId: 1,
-        label: 'All'
-    },
-    {
-        filterId: 2,
-        label: 'Development'
-    },
-    {
-        filterId: 3,
-        label: 'Game'
-    },
-];
-
-const Project = () => {
-    const [filteredvalue, setFilteredValue] = useState(1);
-    const [hoveredValue, setHoveredVlue] = useState(null);
-
-    function handleFilter(currentId) {
-        setFilteredValue(currentId)
-    }
-
-    function handleHover(index) {
-        setHoveredVlue(index)
-    }
-
-
-    const filteredItems = filteredvalue === 1 ? portfolioData : portfolioData.filter(item => item.id === filteredvalue);
-
-    return (
-        <section id="project" className="project">
-            <PageHeaderContent
-                headerText="My Projects" icon={<AiFillProject size={40} />} />
-            <div className="portfolio_content">
-                <ul className="portfolio_content_filter">
-                    {filterData.map((item) => (
-                        <li className={item.filterId === filteredvalue ? "active" : ""} onClick={() => handleFilter(item.filterId)} key={item.filterId}>
-                            {item.label}</li>
-                    ))}
-                </ul>
-                <div className="portfolio_content_cards">{filteredItems.map((item, index) => (<div className="portfolio_content_cards_item" key={`cardItem${item.name.trim()}`}
-                    onMouseEnter={() => handleHover(index)}
-                    onMouseLeave={() => handleHover(null)}>
-                    <div className="portfolio_content_cards_item_img-wrapper">
-                        <a>
-                            <img src={item.image} alt={item.name} />
-                        </a>
-                    </div>
-                    <div className="overlay">
-                        {
-                            index === hoveredValue && (<div>
-                                <p>{item.name}</p>
-                                <a href={item.link || "#"} target="_blank" rel="noopener noreferrer">
-                                    <button>Visit</button>
-                                </a></div>)
-                        }
-                    </div>
-                </div>
-                ))
-                }
-                </div>
-            </div>
-        </section>
-    )
-}
-export default Project;
