039d613332252aaa0ac17f0d5e3668f72f5cf451
Devesh-Shah24 <deveshmuz2020@gmail.com>
2025-04-20 10:20:52 +0530

    Update  commit

--- src/containers/skills/skills.js
+++ src/containers/skills/skills.js
@@ -11,6 +11,10 @@ export const skillsData = [
                 Perscentage: "80",
             },
             {
+                skillName: "SCSS",
+                Perscentage: "89",
+            },
+            {
                 skillName: "JavaScript",
                 Perscentage: "90",
             },
@@ -20,38 +24,38 @@ export const skillsData = [
             },
         ],
     },
-    {
-        label: "Back End",
-        data: [
-            {
-                skillName: "Node.JS",
-                Perscentage: "75",
-            },
-            {
-                skillName: "Express",
-                Perscentage: "79",
-            },
-            // {
-            //     skillName: "Django",
-            //     Perscentage: "90",
-            // },
-        ],
-    },
-    {
-        label: "DataBase",
-        data: [
-            // {
-            //     skillName: "SQL",
-            //     Perscentage: "90",
-            // },
-            {
-                skillName: "Mongo DB",
-                Perscentage: "85",
-            },
-            {
-                skillName: "No-SQL",
-                Perscentage: "80",
-            },
-        ],
-    },
+    // {
+    //     label: "Back End",
+    //     data: [
+    //         {
+    //             skillName: "Node.JS",
+    //             Perscentage: "75",
+    //         },
+    //         {
+    //             skillName: "Express",
+    //             Perscentage: "79",
+    //         },
+    //         // {
+    //         //     skillName: "Django",
+    //         //     Perscentage: "90",
+    //         // },
+    //     ],
+    // },
+    // {
+    //     label: "DataBase",
+    //     data: [
+    //         // {
+    //         //     skillName: "SQL",
+    //         //     Perscentage: "90",
+    //         // },
+    //         {
+    //             skillName: "Mongo DB",
+    //             Perscentage: "85",
+    //         },
+    //         {
+    //             skillName: "No-SQL",
+    //             Perscentage: "80",
+    //         },
+    //     ],
+    // },
 ];
